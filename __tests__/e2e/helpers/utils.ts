964d60ca2c76d03d208851b70a2a40b4a22435d9
Wim Selles <wim@wswebcreation.nl>
2021-12-21 16:03:27 +0100

    test: fix deep linking for real iOS real devices

--- __tests__/e2e/helpers/utils.ts
+++ __tests__/e2e/helpers/utils.ts
@@ -86,20 +86,63 @@ const openDeepLinkUrl = async (url: string): Promise<void | string> => {
     });
   }
 
-  // This works for Simulators, not for real devices so we need to fix that
-  await driver.url(`${prefix}${url}`);
-  // try {
-  //   // Wait for the notification and accept it
-  //   const openSelector =
-  //     "type == 'XCUIElementTypeButton' && name CONTAINS 'Open'";
-  //   const openButton = await $(`-ios predicate string:${openSelector}`);
-  //   await openButton.waitForDisplayed({timeout: 6000});
-  //
-  //   return openButton.click();
-  // } catch (e) {
-  //   console.log('openDeepLinkUrl error = ', e);
-  //   return;
-  // }
+  // We can use `driver.url` on iOS simulators, but not on iOS real devices. The reason is that iOS real devices
+  // open Siri when you call `driver.url('')` to use a deep link. This means that real devices need to have a different implementation
+  // then iOS sims
+  // iOS sims and real devices can be distinguished by their UDID. Based on these sources there is a diff in the UDIDS
+  // - https://blog.diawi.com/2018/10/15/2018-apple-devices-and-their-new-udid-format/
+  // - https://www.theiphonewiki.com/wiki/UDID
+  // iOS sims have more than 1 `-` in the UDID and the UDID is being
+  const simulatorRegex = new RegExp('(.*-.*){2,}');
+
+  // Check if we are a simulator
+  if (
+    'udid' in driver.capabilities &&
+    !simulatorRegex.test(driver.capabilities.udid as string)
+  ) {
+    await driver.url(`${prefix}${url}`);
+  } else {
+    // Else we are a real device and we need to take some extra steps
+    // Launch Safari to open the deep link
+    await driver.execute('mobile: launchApp', {
+      bundleId: 'com.apple.mobilesafari',
+    });
+
+    // Add the deep link url in Safari in the `URL`-field
+    // This can be 2 different elements, or the button, or the text field
+    // Use the predicate string because  the accessibility label will return 2 different types
+    // of elements making it flaky to use. With predicate string we can be more precise
+    const addressBarSelector =
+      "name CONTAINS 'URL' OR name CONTAINS 'TabBarItemTitle'";
+    const urlFieldSelector =
+      'type == "XCUIElementTypeTextField" && name CONTAINS "URL"';
+    const addressBar = $(`-ios predicate string:${addressBarSelector}`);
+    const urlField = $(`-ios predicate string:${urlFieldSelector}`);
+
+    // Wait for the url button to appear and click on it so the text field will appear
+    // iOS 13 now has the keyboard open by default because the URL field has focus when opening the Safari browser
+    if (!(await driver.isKeyboardShown())) {
+      await addressBar.waitForDisplayed();
+      await addressBar.click();
+    }
+
+    // Submit the url and add a break
+    await urlField.setValue(`${prefix}${url}\uE007`);
+  }
+
+  // Wait for the notification and accept it
+  // When using an iOS simulator you will only get the pop-up once, all the other times it won't be shown
+  try {
+    const openSelector =
+      "type == 'XCUIElementTypeButton' && name CONTAINS 'Open'";
+    const openButton = $(`-ios predicate string:${openSelector}`);
+    // Assumption is made that the alert will be seen within 2 seconds, if not it did not appear
+    await openButton.waitForDisplayed({timeout: 3000});
+    await openButton.click();
+  } catch (e) {
+    // ignore
+    console.log('Deeplink error = ', e);
+  }
 };
 
 export {
